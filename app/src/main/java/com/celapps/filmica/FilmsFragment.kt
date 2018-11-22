015bb881f2f491cd058bd63c2f2089405f1faa48
Cristian Espes <christian_zgza@hotmail.com>
2018-11-22 20:53:21 +0100

    Extracción de la vista de error del listado de Películas.

--- app/src/main/java/com/celapps/filmica/FilmsFragment.kt
+++ app/src/main/java/com/celapps/filmica/FilmsFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_films.*
+import kotlinx.android.synthetic.main.layout_error.*
 
 class FilmsFragment: Fragment() {
 
