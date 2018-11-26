527c82aecd87293543780c29822bc136b7286f2d
Cristian Espes <christian_zgza@hotmail.com>
2018-11-26 21:49:28 +0100

    Incluido el adaptador en el fragment de Watchlist.

--- app/src/main/java/com/celapps/filmica/view/watchlist/WatchlistFragment.kt
+++ app/src/main/java/com/celapps/filmica/view/watchlist/WatchlistFragment.kt
@@ -6,10 +6,16 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_watchlist.*
 
 import com.celapps.filmica.R
 
 class WatchlistFragment : Fragment() {
+
+    val adapter: WatchlistAdapter by lazy {
+        val instance = WatchlistAdapter()
+        instance
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +23,12 @@ class WatchlistFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_watchlist, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        watchlist.adapter = adapter
     }
 
 }
