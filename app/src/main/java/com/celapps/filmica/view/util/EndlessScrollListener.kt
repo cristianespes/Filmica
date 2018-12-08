ed0ef25295d929af9d6a93a627e06ac04e59003f
Cristian Espes <christian_zgza@hotmail.com>
2018-12-08 12:16:21 +0100

    Añadida barra de progreso durante la carga de nuevos items en los listados Discover y Trending.

--- app/src/main/java/com/celapps/filmica/view/util/EndlessScrollListener.kt
+++ app/src/main/java/com/celapps/filmica/view/util/EndlessScrollListener.kt
@@ -10,7 +10,7 @@ abstract class EndlessScrollListener(layoutManager: RecyclerView.LayoutManager) 
 
     private var previousTotal = 0 // The total number of items in the dataset after the last load
     private var loading = true // True if we are still waiting for the last set of data to load.
-    private val visibleThreshold = 5 // The minimum amount of items to have below your current scroll position before loading more.
+    private val visibleThreshold = 2 // The minimum amount of items to have below your current scroll position before loading more.
     private var firstVisibleItem: Int = 0
     private var visibleItemCount: Int = 0
     private var totalItemCount: Int = 0
