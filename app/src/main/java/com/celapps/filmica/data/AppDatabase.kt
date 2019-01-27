175f08fde0bf790e7ac3bec3fa13160f89e6cf61
Cristian Espes <christian_zgza@hotmail.com>
2019-01-27 18:56:35 +0100

    Corrección en la migración de la base de datos.

--- app/src/main/java/com/celapps/filmica/data/AppDatabase.kt
+++ app/src/main/java/com/celapps/filmica/data/AppDatabase.kt
@@ -13,6 +13,6 @@ abstract class AppDatabase: RoomDatabase() {
 
 val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        database.execSQL("ALTER TABLE Film ADD COLUMN backdrop STRING")
+        database.execSQL("ALTER TABLE Film ADD COLUMN backdrop TEXT")
     }
 }
